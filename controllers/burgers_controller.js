d3801b3c33ee959b5c8e07bb73cc196f445cc5f6
earyzhikova <elenaryzh55@gmail.com>
2018-05-01 17:09:35 -0400

    Burger added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,41 +23,22 @@ router.post("/api/burgers", function(req, res) {
   burger.create([
     "burger_name", "devoured"
   ], [
-    req.body.burger, req.body.devoured
+    req.body.burger, 0
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers", function(req, res) {
-  // var condition = "id = " + req.params.id;
-
-  // console.log("condition", condition);
-
-  // burger.update({
-  //   devoured: req.body.devoureds
-  // }, condition, function(result) {
-  //   if (result.changedRows == 0) {
-  //     // If no rows were changed, then the ID must not exist, so 404
-  //     return res.status(404).end();
-  //   } else {
-  //     res.status(200).end();
-  //   }
-  // });
-
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
- burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+router.post("/api/devoured", function(req, res) {
+  console.log(req.body.dev);
+  burger.update([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger, 1
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
   });
 });
 
