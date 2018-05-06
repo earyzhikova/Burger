5df05c3c9cf4cd1bd7903dbcfe9cc7b37f0ee6d2
earyzhikova <elenaryzh55@gmail.com>
2018-05-06 18:53:51 -0400

    Read Me

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ CREATE TABLE `burgers` (
   `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
   `burger_name` VARCHAR( 255) NOT NULL,
   `devoured` BOOLEAN default FALSE,
-
+  `createAt`  CURRENT_TIMESTAMP,
   PRIMARY KEY ( `id` ) 
 );
